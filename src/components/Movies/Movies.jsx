8ef42a94409a1a232b60a2082b7eafa8513467d0
Patrick Bialy <patrickbialy@yahoo.com>
2022-12-01 15:45:28 -0500

    Rendering Movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from 'react';
-// import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
-// import { useSelector } from 'react-redux';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { MovieList } from '..';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies found.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+  if (error) return 'An error has occurred';
+
   return (
     <div>
-      Movies
+      <MovieList movies={data} />
     </div>
   );
 };
