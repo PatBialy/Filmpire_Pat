2d6993b43541dbbd21a1951b839e8d20c132df2e
Patrick Bialy <patrickbialy@yahoo.com>
2022-12-03 19:20:51 -0500

    Logout Functionality

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
-import { ClassNames } from '@emotion/react';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 import { useSelector } from 'react-redux';
 import { userSelector } from '../../features/auth';
 import useStyles from './styles';
@@ -7,11 +8,23 @@ import useStyles from './styles';
 const Profile = () => {
   const classes = useStyles();
   const { user } = useSelector(userSelector);
+  const favoriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
   console.log(user);
   return (
-    <div className={classes.yo}>
-      Profile - {user.username}
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between" className={classes.yo}>
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? <Typography variant="h5">Add Favorites Or Watchlist Movies To See Them Here!</Typography> : <Box>Favorite Movies</Box>}
+    </Box>
   );
 };
 
